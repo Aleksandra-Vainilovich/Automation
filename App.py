b7b17b6c3dbbe1ab8cdb03f77667113f24da4e09
Aleksandra_Vainilovich <Aleksandra_Vainilovich@epam.com>
2019-11-26 18:54:33 +0300

    word count added

--- App.py
+++ App.py
@@ -19,12 +19,6 @@ file_name = ('Example.fb2')
 data = open(path, encoding="utf8")
 data_contents = data.read()
 #print(data_contents)
-#book_name = re.match("(<book-title>\w+)\W(</book-title>\w+)",data_contents)
-#book_name = re.findall("<book-title>\w+",data_contents)
-#book_name_end = re.findall(r"<\book\-\title\>(.*)\<\book\-\title\>",data_contents)
-#book_name = re.search(r"<book-title>\(([^)(]*)\)</book-title>",data_contents)
-#print('Book name ' ,book_name)
-#print('Book name ' ,book_name_end)
 
 print('words count')
 words = data_contents.split(" ")
@@ -40,36 +34,22 @@ print((data_contents.split(start))[2].split(end)[0])
 print('read all files in folder')
 file_names = os.listdir('C:\Automation\Input')
 print(file_names)
-print('C:\Automation\Input', file_names)
 pth = 'C:\Automation\Input\*.fb2'
 nms = glob.glob(pth)
 print(nms)
 for files_to_read in nms:
-    #if files_to_read.endswith('.fb2'):
     if fnmatch.fnmatch(files_to_read, '*.fb2'):
         op_file = open(files_to_read, encoding="utf8")
         data_to_read = op_file.read()
         print(data_to_read)
-print(files_to_read)
+#print(files_to_read)
+
+print('all files words count')
+for file_word_count in nms:
+    op_file = open(file_word_count, encoding="utf8")
+    data_to_read = op_file.read()
+    words = data_to_read.split(" ")
+    count_words = len(words)
+    print('Number of words ', count_words)
 
 
-words = data_contents.split(" ")
-count_words = len(words)
-print('Number of words ', count_words)
-#with open('Example.fb2','r', encoding="utf8") as f:
-    #file_body = f.read()
-    #wordcount = len(file_body,split())
-#print(wordcount)
-
-#path = 'C:\Automation\Input\*.fb2'
-#files = glob.glob(path)
-#print(files)
-#for name in files
-
-path = 'C:/Automation/Input/*.fb2'
-files = os.listdir('C:\Automation\Input')
-print(files)
-for file in os.listdir('C:\Automation\Input'):
-    with open(file,"r") as f:
-        for line in f.readlines():
-            print(line.split())
