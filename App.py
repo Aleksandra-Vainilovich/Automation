cf127b585728b8ed2e9f3080d5625540474dbc68
Aleksandra <Aleksandra_Vainilovich@epam.com>
2019-12-01 16:35:25 +0300

    scripts modified

--- App.py
+++ App.py
@@ -3,8 +3,12 @@
 import os
 import shutil
 import fnmatch
-import glob #, re, errno, sys
+import glob
+import sqlite3
+#, re, errno, sys
 #os.chdir('c:\Automation\Input')
+
+
 
 print('move files with incorrect ')
 files = os.listdir('C:\\Automation\\Input')
@@ -41,8 +45,7 @@ for files_to_read in nms:
     if fnmatch.fnmatch(files_to_read, '*.fb2'):
         op_file = open(files_to_read, encoding="utf8")
         data_to_read = op_file.read()
-        print(data_to_read)
-#print(files_to_read)
+        #print(data_to_read)
 
 print('all files words count')
 for file_word_count in nms:
@@ -50,6 +53,81 @@ for file_word_count in nms:
     data_to_read = op_file.read()
     words = data_to_read.split(" ")
     count_words = len(words)
-    print('Number of words ', count_words)
+    print( count_words)
+
+print('book title')
+for files_to_read_titles in nms:
+    if fnmatch.fnmatch(files_to_read_titles, '*.fb2'):
+        op_file = open(files_to_read_titles, encoding="utf8")
+        data_to_read = op_file.read()
+        start = '<book-title>'
+        end = '</book-title>'
+        print((data_to_read.split(start))[1].split(end)[0])
+
+print('number_of_paragraph')
+for files_to_read_titles in nms:
+    if fnmatch.fnmatch(files_to_read_titles, '*.fb2'):
+        op_file = open(files_to_read_titles, encoding="utf8")
+        data_to_read = op_file.read()
+        paragraphs = data_to_read.split("</p>")
+        count_paragraphs = len(paragraphs)
+        print(count_paragraphs)
+
+print('number_of_letters')
+for number_of_letters in nms:
+    if fnmatch.fnmatch(number_of_letters, '*.fb2'):
+        op_file = open(number_of_letters, encoding="utf8")
+        data_to_read = op_file.read()
+        count_letters = len(data_to_read)
+        print(count_letters)
+
+
+print('capital letters')
+for words_in_file in nms:
+    if fnmatch.fnmatch(words_in_file, '*.fb2'):
+        op_file = open(words_in_file, encoding="utf8")
+        data_to_read = op_file.read()
+        #words = data_to_read.split(" ")
+        #print(data_to_read)
+
+#hash = {} #initialize an empty dictinonary
+count1 = 0
+count2 = 0
+for word in words_in_file.split(" "):
+    if word[0].islower():
+        count1 = count1+1
+    elif word[0].upper():
+        count2 = count2+1
+print(count1)
+print(count2)
+
+    #if word[0].isupper():
+        #if word in hash:
+            #hash[word] +=1
+print('word')
+
+
+
+print('insert into table')
+
+conn = sqlite3.connect('Automation.db')
+c = conn.cursor()
+
+def data_entry():
+    book_name = files_to_read_titles
+    number_of_words = file_word_count
+    c.execute("INSERT INTO for_all_files(book_name, number_of_paragraph, number_of_words, number_of_letters) VALUES (?,?,?,?)",
+              (book_name, files_to_read_titles, number_of_words, number_of_letters))
+    conn.commit()
+
+def read_from_db():
+    c.execute('SELECT * FROM for_all_files')
+    #data = c.fetchall()
+    #print(data)
+    for row in c.fetchall():
+        print(row)
+
+c.close()
+conn.close()
 
 
--- Create_table.py
+++ Create_table.py
@@ -1,13 +1,13 @@
 import sqlite3
 
-conn=sqlite3.connect('Automation.db')
+conn = sqlite3.connect('Automation.db')
 c = conn.cursor()
 
 def create_table():
-    c.execute('CREATE TABLE IF NOT EXISTS for_all_files (book_name TEXT, number_of_paragraph REAL, number_of_words REAL, words_with_capital_letters REAL, words_in_lowercase REAL)')
+    c.execute('CREATE TABLE IF NOT EXISTS for_all_files ( book_name TEXT, number_of_paragraph REAL, number_of_words REAL, number_of_letters REAL, words_with_capital_letters REAL)')
 
 def data_entry():
-    c.execute("INSERT INTO for_all_files VALUES ('good', 56, 65, 76, 788)")
+    c.execute("INSERT INTO for_all_files VALUES ('good', 56, 766, 65, 788)")
     conn.commit()
     c.close()
     conn.close()
